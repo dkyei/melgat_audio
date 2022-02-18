fc47608743015454dc10d219473d2cb742dda9fe
dkyei <dykyei@st.ug.edu.gh>
2022-02-18 21:39:08 +0000

    my first message

--- AudioPlayer/AudioPlayer/settings.py
+++ AudioPlayer/AudioPlayer/settings.py
@@ -28,10 +28,10 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(=(#d5vup7be_4(rlsprmi+j*(q)h3doqndqyqq6m3m8tu@g@0'
+SECRET_KEY = 'django-insecure-(=(#d5vup7be_4(rlsprmi+j*(q)h3doqndqyqq6m3m8tu@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
